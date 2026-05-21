b57ac4cadb1cd140917f8a294637ce0ac93e4466
Memory Care <user@memorycare.com>
2026-05-21 20:40:31 +0530

    Add real Firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,29 +1,18 @@
 import { initializeApp, getApps } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence, getAuth } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { Platform } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCod6G5jKHMkNV3XyEPKvaEUcYpb227-zg",
+  authDomain: "dementia-a06cd.firebaseapp.com",
+  projectId: "dementia-a06cd",
+  storageBucket: "dementia-a06cd.firebasestorage.app",
+  messagingSenderId: "85788968871",
+  appId: "1:85788968871:web:694070eb4ede4fe5435882",
+  measurementId: "G-JEZ8L03Y0J",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-let auth;
-if (Platform.OS === 'web') {
-  auth = getAuth(app);
-} else {
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
-}
-
-export { auth };
+export const auth = getAuth(app);
 export const db = getFirestore(app);
