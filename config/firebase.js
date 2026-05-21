0fb55f034c708c65b8059abfc675ca9c20f709ca
Memory Care <user@memorycare.com>
2026-05-21 23:31:15 +0530

    Update Firebase config - new project dementia-care-56569

--- config/firebase.js
+++ config/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "AIzaSyCod6G5jKHMkNV3XyEPKvaEUcYpb227-zg",
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "dementia-a06cd.firebaseapp.com",
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "dementia-a06cd",
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "dementia-a06cd.firebasestorage.app",
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "85788968871",
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "1:85788968871:web:694070eb4ede4fe5435882",
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-JEZ8L03Y0J",
+  apiKey: "AIzaSyBUJvpnViwEEWS_UXWMYDAHHmZMEOxLggc",
+  authDomain: "dementia-care-56569.firebaseapp.com",
+  projectId: "dementia-care-56569",
+  storageBucket: "dementia-care-56569.firebasestorage.app",
+  messagingSenderId: "1003875842780",
+  appId: "1:1003875842780:web:84965392036b92e6ff9219",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
